9f224d14812a1ad49481d5c70f13cc5d1406bc82
Martin <martyofmca@gmail.com>
2023-08-13 14:09:56 -0700

    fix: test case for Base Model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -17,13 +17,16 @@ class TestBaseModel(TestCase):
         """ Test the save method """
         obj = BaseModel()
         updated_at = datetime.now()
+        first_update = obj.updated_at
         # Compare before calling save
         self.assertEqual(obj.updated_at.date(), updated_at.date())
         obj.save()
         updated_at = datetime.now()
+        second_update = obj.updated_at
         # Compare after calling save
         self.assertEqual(obj.updated_at.date(), updated_at.date())
         self.assertTrue(isfile("file.json"))
+        self.assertLess(first_update, second_update, "Error")
 
     def test_to_dict(self):
         """ Test the to_dict method """
@@ -31,21 +34,3 @@ class TestBaseModel(TestCase):
         obj = BaseModel()
         actual = obj.to_dict()
         self.assertEqual(sorted(tuple(actual.keys())), sorted(expected))
-
-    def test_obj_with_kwargs_to_dict(self):
-        """
-        Test the to_dict method for BaseModel object
-        created with key-worded arguments
-        """
-        expected = ("id", "created_at", "updated_at", "__class__", "name")
-        obj = BaseModel(**{"id": "398c7f36-10a2-4b69-9e28-d9ccf0dbde37",
-                        "created_at": datetime.now().isoformat(),
-                        "updated_at": datetime.now().isoformat(),
-                        "name": "Jake Savage"})
-        self.assertEqual(sorted(tuple(obj.to_dict().keys())), sorted(expected))
-
-    def test_str(self):
-        """ Test the __str__ method """
-        obj = BaseModel()
-        id = obj.id
-        self.assertEqual(str(obj), f"[BaseModel] ({id}) {obj.__dict__}")
