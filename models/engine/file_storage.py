0021baed2045c7de512a281e94740f8ced46bf13
Martin <martyofmca@gmail.com>
2023-08-12 22:03:57 -0700

    fix: imports for new models

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -43,6 +43,11 @@ class FileStorage:
                 objects = loads(file.read())
             from models.base_model import BaseModel
             from models.user import User
+            from models.state import State
+            from models.amenity import Amenity
+            from models.city import City
+            from models.place import Place
+            from models.review import Review
             for key, value in objects.items():
                 class_name = value["__class__"]
                 del value["__class__"]
