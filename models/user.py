80fbd87a57ab90803fb2b09bd39975b3c779c849
Martin <martyofmca@gmail.com>
2023-08-12 21:15:38 -0700

    add User model

---
+++ models/user.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Defines a module for the User class
+"""
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """
+    Represents an instance of a user
+    """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
