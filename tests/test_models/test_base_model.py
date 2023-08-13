8014d35c92a75a05cdfe985c5258f21286aa2577
Martin <martyofmca@gmail.com>
2023-08-13 06:39:40 -0700

    fix: test case for File Storage class

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,47 +1,17 @@
 #!/usr/bin/python3
 """
-Defines unit tests for the Base Model class.
+Defines unit tests for the Amenity class
 """
 from unittest import TestCase
-from uuid import uuid4
-from datetime import datetime, date
+from datetime import datetime
 from models.base_model import BaseModel
+from os.path import isfile
 
 
 class TestBaseModel(TestCase):
     """
-    Test class with methods testing the Base Class
+    Test class with methods testing the Amenity class
     """
-
-    def test_init_without_args(self):
-        """ Test object creation without any arguments """
-        created_at = datetime.now()
-        updated_at = datetime.now()
-        obj = BaseModel()
-        self.assertEqual(obj.created_at.date(), created_at.date())
-        self.assertEqual(obj.updated_at.date(), created_at.date())
-
-    def test_init_with_args(self):
-        """ Test object creation with key-worded arguments """
-        create = date(2000, 12, 31).isoformat()
-        update = date(2001, 1, 1).isoformat()
-        id = "60efac0e-85aa-47e0-91e1-dfb0daafacc6"
-        obj_dict = {"id": id, "created_at": create, "updated_at": update}
-        obj = BaseModel(**obj_dict)
-        self.assertEqual(obj.id, id)
-        self.assertEqual(obj.created_at, datetime.fromisoformat(create))
-        self.assertEqual(obj.updated_at, datetime.fromisoformat(update))
-
-    def test_str(self):
-        """ Test the __str__ method """
-        id = uuid4()
-        created_at = datetime.now()
-        updated_at = datetime.now()
-        obj = BaseModel()
-        actual = str(obj)
-        self.assertIs(type(actual), str)
-        self.assertEqual(obj.created_at.date(), created_at.date())
-        self.assertEqual(obj.updated_at.date(), updated_at.date())
 
     def test_save(self):
         """ Test the save method """
@@ -53,6 +23,7 @@ class TestBaseModel(TestCase):
         updated_at = datetime.now()
         # Compare after calling save
         self.assertEqual(obj.updated_at.date(), updated_at.date())
+        self.assertTrue(isfile("file.json"))
 
     def test_to_dict(self):
         """ Test the to_dict method """
@@ -60,3 +31,21 @@ class TestBaseModel(TestCase):
         obj = BaseModel()
         actual = obj.to_dict()
         self.assertEqual(sorted(tuple(actual.keys())), sorted(expected))
+
+    def test_obj_with_kwargs_to_dict(self):
+        """
+        Test the to_dict method for BaseModel object
+        created with key-worded arguments
+        """
+        expected = ("id", "created_at", "updated_at", "__class__", "name")
+        obj = BaseModel(**{"id": "398c7f36-10a2-4b69-9e28-d9ccf0dbde37",
+                        "created_at": datetime.now().isoformat(),
+                        "updated_at": datetime.now().isoformat(),
+                        "name": "Jake Savage"})
+        self.assertEqual(sorted(tuple(obj.to_dict().keys())), sorted(expected))
+
+    def test_str(self):
+        """ Test the __str__ method """
+        obj = BaseModel()
+        id = obj.id
+        self.assertEqual(str(obj), f"[BaseModel] ({id}) {obj.__dict__}")
