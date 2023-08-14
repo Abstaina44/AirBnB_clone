6eec9961288e680bc953aa68eeb4d0b65e0bfad4
Martin <martyofmca@gmail.com>
2023-08-13 18:55:32 -0700

    fix: text cases for other models

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -12,36 +12,17 @@ class TestPlace(TestCase):
     Test class with methods testing the Place class
     """
 
-    def test_init(self):
-        """ Test place object creation """
-        created_at = datetime.now()
+    def test_new_instance(self):
+        """ Test the creation of Place instance """
         obj = Place()
-        self.assertEqual(obj.created_at.date(), created_at.date())
-
-    def test_str(self):
-        """ Test the __str__ method """
-        created_at = datetime.now()
-        updated_at = datetime.now()
-        obj = Place()
-        actual = str(obj)
-        self.assertIs(type(actual), str)
-        self.assertEqual(obj.created_at.date(), created_at.date())
-        self.assertEqual(obj.updated_at.date(), updated_at.date())
-
-    def test_save(self):
-        """ Test the save method """
-        obj = Place()
-        updated_at = datetime.now()
-        # Compare before calling save
-        self.assertEqual(obj.updated_at.date(), updated_at.date())
-        obj.save()
-        updated_at = datetime.now()
-        # Compare after calling save
-        self.assertEqual(obj.updated_at.date(), updated_at.date())
-
-    def test_to_dict(self):
-        """ Test the to_dict method """
-        expected = ("id", "created_at", "updated_at", "__class__")
-        obj = Place()
-        actual = obj.to_dict()
-        self.assertEqual(sorted(tuple(actual.keys())), sorted(expected))
+        self.assertEqual(obj.city_id, "")
+        self.assertEqual(obj.user_id, "")
+        self.assertEqual(obj.name, "")
+        self.assertEqual(obj.description, "")
+        self.assertEqual(obj.number_rooms, 0)
+        self.assertEqual(obj.number_bathrooms, 0)
+        self.assertEqual(obj.max_guest, 0)
+        self.assertEqual(obj.price_by_night, 0)
+        self.assertEqual(obj.latitude, 0.0)
+        self.assertEqual(obj.longitude, 0.0)
+        self.assertEqual(obj.amenity_ids, [])
