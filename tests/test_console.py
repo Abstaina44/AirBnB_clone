2782abff451e73a05a7034fc4f9cc793d82ed498
Martin <martyofmca@gmail.com>
2023-08-15 19:39:27 -0700

    add test cases for console

---
+++ tests/test_console.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Defines unit tests for the console """
+from unittest import TestCase
+from console import HBNBCommand
+
+
+class TestConsole(TestCase):
+    """ Test class with method testing console """
+    pass
